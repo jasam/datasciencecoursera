3bca0ca30089856960c0092cb6ab9b2e36877dff
Javier Rey <jreyro@gmail.com>
2014-12-09 18:23:33 -0500

    Fix bug

--- pollutantmean.R
+++ pollutantmean.R
@@ -21,7 +21,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   files = dir(directory)
   
   ## Transform file names into number
-  idsFiles : 1:332
+  idsFiles = 1:332
   
   ## Select only files about parameter selected
   files = files[idsFiles %in% id]
