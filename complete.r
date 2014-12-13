a0e0d30698620378c4fe65785f8384e4ec5e3ae8
Jasam <jreyro@gmail.com>
2014-12-13 10:29:17 -0500

    Fix errors

--- complete.r
+++ complete.r
@@ -18,24 +18,15 @@ complete <- function(directory, id = 1:332) {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   
-  ranges = id 
-  
   ## List files from directory 
   files = dir(directory)
   
-  ## Transform file names into number
-  idsFiles = 1:332
-  
-  ## Select only files about parameter selected
-  files = files[idsFiles %in% id]
-  
   observations <- data.frame(id = numeric(), nobs = numeric())
 
-  for(i in seq_along(ranges)) {
-	position = ranges[i]
+  for(i in id) {
 	dataFiles = lapply(file.path(directory,files[i]),read.csv)
 	dataFrame = do.call(rbind.data.frame,dataFiles)
-	observations <- rbind(observations, data.frame(id = position, nobs = sum(complete.cases(dataFrame))))
+	observations <- rbind(observations, data.frame(id = i, nobs = sum(complete.cases(dataFrame))))
   } 
   observations
 }
