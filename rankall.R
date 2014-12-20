b4039e6427138d4c29064db81729c9683bd2b2bc
Jasam <jreyro@gmail.com>
2014-12-20 16:34:40 -0500

    Fix bugs

--- rankall.R
+++ rankall.R
@@ -1,4 +1,5 @@
 rankall <- function(outcome, num = "best") {
+    
     ## Read outcome data
     ## Check that state and outcome are valid
     ## For each state, find the hospital of the given rank
@@ -10,27 +11,72 @@ rankall <- function(outcome, num = "best") {
     infoHospitals[, 17] <- suppressWarnings(as.numeric(infoHospitals[, 17]))
     infoHospitals[, 23] <- suppressWarnings(as.numeric(infoHospitals[, 23]))
     
-    validState = state %in% unique(infoHospitals[,7])
+    observations <- data.frame(observation = character())
+    observations <- rbind(observations, data.frame(observation = "heart failure"))
+    observations <- rbind(observations, data.frame(observation = "heart attack"))
+    observations <- rbind(observations, data.frame(observation = "pneumonia"))
     
-    states = unique(infoHospitals[,7])
+    ## Is a valid outcome
+    validOutCome = outcome %in% observations[,1]
     
-    rankingAll <- data.frame(hospital = character(), state = character())ñ
+    if (!validOutCome){
+        stop("invalid outcome")
+    }
+    
+    ## Order States
+    states = sort(unique(infoHospitals[,7]))
+    
+    rankingAll <- data.frame(hospital = character(), state = character())
     
     HEAD_VALUE = 1
     
-    for (i in states){
-        print(i)
+    for (i in states) {
         subSetState = subset(infoHospitals, State==i)
-        ranking = subSetState[order(subSetState[,2], subSetState[,11]),c(2,11)] 
+        
+        if (outcome == "heart failure"){
+            ranking = subSetState[order(subSetState[,2], subSetState[,17]),c(2,17)]  
+        } 
+        
+        if (outcome == "heart attack"){
+            ranking = subSetState[order(subSetState[,2], subSetState[,11]),c(2,11)]  
+        } 
+        
+        if (outcome == "pneumonia"){
+            ranking = subSetState[order(subSetState[,2], subSetState[,23]),c(2,23)] 
+        }
         
         ranking = transform(ranking, rank = ave(ranking[,2], 
                                                 FUN = function(x) rank(x, ties.method = "first")))
         
         ranking = ranking[order(ranking[,3]),c(1,2,3)]
         
-        hospitalRank = ranking[ranking$rank == 20, c(1)]
+        if (num == "best") {
+            hospitalRank = ranking[ranking$rank == HEAD_VALUE, c(1)] 
+        }
         
-        rankingAll <- rbind(rankingAll, data.frame(hospital = hospitalRank, state = i))
+        else if (num == "worst") {
+            ## Complete cases only
+            ranking =  ranking[complete.cases(ranking),]
+            tailRank = tail(ranking[3], n=1)
+            hospitalRank = ranking[ranking$rank == tailRank[,1], c(1)] 
+        }
+        
+        else {
+            hospitalRank = ranking[ranking$rank == num, c(1)] 
+        }
+        
+        
+        if (length(hospitalRank) == 0) {
+            rankingAll <- rbind(rankingAll, data.frame(hospital = NA, state = i))
+            
+        }
+        
+        else {
+            
+            rankingAll <- rbind(rankingAll, data.frame(hospital = hospitalRank, state = i))
+        }              
         
     }
+    
+    rankingAll
 }
